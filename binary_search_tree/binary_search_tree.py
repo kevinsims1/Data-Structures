4164a93275a59504b58ea9fb7648194e0a07af09
kevinsims1 <kevinsims1@outlook.com>
2019-12-20 09:45:50 -0600

    finishing up on in order print

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -24,10 +24,6 @@ class BinarySearchTree:
                 return
             else:
                 self.left.insert(value)
-            
-
-        
-
 
     # Return True if the tree contains the value
     # False if it does not
@@ -46,9 +42,6 @@ class BinarySearchTree:
                 else:
                     return False
             
-
-            
-
     # Return the maximum value found in the tree
     def get_max(self):
         if not self.right:
@@ -59,18 +52,42 @@ class BinarySearchTree:
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
-        cb(self.value)
-        if self.right:
-            self.right.for_each(cb)
-        if self.left:
-            self.left.for_each(cb)
+        # cb(self.value)
+        # if self.right:
+        #     self.right.for_each(cb)
+        # if self.left:
+        #     self.left.for_each(cb)
+        stack = Stack()
+        stack.push(self)
+        while stack.len() > 0:
+            current_node = stack.pop()
+            if current_node.right:
+                stack.push(current_node.right)
+            if current_node.left:
+                stack.push(current_node.left)
+            cb(current_node.value)
+
 
     # DAY 2 Project -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        return
+        #Recursive
+        stack = Stack()
+        stack.push(node)
+        while stack.len() > 0:
+            current_node = stack.pop()
+            if current_node.left is None and current_node.right is None:
+                print(current_node.value)
+            else:
+                if current_node.left is None:
+                    print(current_node.value)
+                else:
+                    current_node.dft_print(current_node.left)
+
+                if current_node.right:
+                    current_node.dft_print(current_node.right)
         
         
 
@@ -78,17 +95,63 @@ class BinarySearchTree:
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self, node):
-        return
-
-
-
+        queue = Queue()
+        queue.enqueue(self)
+        while queue.size > 0:
+            current_value = queue.dequeue()
+            print(current_value.value)
+            if current_value.left:
+                queue.enqueue(current_value.left)
+            if current_value.right:
+                queue.enqueue(current_value.right)
+        #make a queue
+        #put root in queue
+        #while queue > 0
+            #pop out front of queue
+            #callBack
+            #if left add to queue
+            # if right add to queue
 
 
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self, node):
-        return
+        # iterative
+        stack = Stack()
+        stack.push(self)
+        while stack.len() > 0:
+            current_node = stack.pop()
+            print(current_node.value)
+            if current_node.left:
+                stack.push(current_node.left)
+            if current_node.right:
+                stack.push(current_node.right)
+        
+        #Recursive
+        # stack = Stack()
+        # stack.push(node)
+        # while stack.len() > 0:
+        #     print(stack.pop().value)
+        #     if node.right:
+        #         node.dft_print(node.right)
+        #     if node.left:
+        #         node.dft_print(node.left)
+
+        #steps
+        # Make a stack
+        # put the root node in the stack
+        # #while stack not empty
+        # pop the root out of stack
+        # callBack
+        # if right add right to stack
+        # if left add left to stack
+
+
+
+
+
+
             
 
     # STRETCH Goals -------------------------
